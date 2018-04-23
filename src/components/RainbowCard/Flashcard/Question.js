3515103eced5de58cb96bac5f73202a16237fc17
Michael Lee <mleenavy@gmail.com>
2018-04-23 08:39:22 -0700

    stubbed Question

---
+++ src/components/RainbowCard/Flashcard/Question.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+export default class Question extends Component {
+
+  render() {
+    const { realm } = this.props
+    return (
+
+    )
+  }
+}
